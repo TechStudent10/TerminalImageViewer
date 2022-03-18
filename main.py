17c56f74c30ac72f053a32ed3e34d297bf3722d0
TechStudent11 <76978184+TechStudent11@users.noreply.github.com>
2022-03-18 07:47:09 -0400

    remove: unused code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from PIL import Image
 from sty import fg, bg
 
-import webcolors
 import sys
 import string
 import random
@@ -13,24 +12,6 @@ if len(args) > 1:
     image = args[1]
 else:
     raise ValueError("Image not provided") 
-
-def closest_colour(requested_colour):
-    min_colours = {}
-    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
-        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
-        rd = (r_c - requested_colour[0]) ** 2
-        gd = (g_c - requested_colour[1]) ** 2
-        bd = (b_c - requested_colour[2]) ** 2
-        min_colours[(rd + gd + bd)] = name
-    return min_colours[min(min_colours.keys())]
-
-def get_colour_name(requested_colour):
-    try:
-        closest_name = actual_name = webcolors.rgb_to_name(requested_colour)
-    except ValueError:
-        closest_name = closest_colour(requested_colour)
-        actual_name = None
-    return actual_name, closest_name
 
 try:
     log = sys.argv[2] == "log"
